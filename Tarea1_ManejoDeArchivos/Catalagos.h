78f4d9f35ba56c5a65172d98ed9e2b1a2c801a0e
Carlos Emiliano Castro Trejo <carlos_castro5156@outlook.es>
2019-01-15 23:18:29 -0600

    Project was created, excel files were added to the folder. Some queries are implemented. Adding, erasing and modification is still missing,:

---
+++ Tarea1_ManejoDeArchivos/Catalagos.h
@@ -0,0 +1,134 @@
+#include<string>
+#include<fstream>
+#include <locale.h>
+#include <clocale>
+#include<iostream>
+
+using namespace std; 
+
+void MostrarCatalogos() 
+{
+	int catalogo = 0;
+	cout << "¿Que catalogo quieres ver?" << endl;
+	cout << "1.- Materias" << endl;
+	cout << "2.- Grupos" << endl;
+	cout << "3.- Profesores" << endl;
+	cin >> catalogo;
+	cout << endl;
+	switch (catalogo)
+	{
+		case(1):
+		{
+			ifstream archivoMaterias;
+			string dato;
+			archivoMaterias.open("MateriasCVA.csv");
+
+			while (!archivoMaterias.eof()) 
+			{
+				getline(archivoMaterias,dato);
+				cout << dato << endl;
+			}
+			archivoMaterias.close();	
+			break;
+		}
+		case(2):
+		{
+			ifstream archivoGrupos;
+			string dato;
+			archivoGrupos.open("GruposCVA.csv");
+
+			while (!archivoGrupos.eof())
+			{
+				getline(archivoGrupos, dato);
+				cout << dato << endl;
+			}
+			archivoGrupos.close();
+			break;
+		}
+		case(3):
+		{
+			ifstream archivoProfesores;
+			string dato;
+			archivoProfesores.open("ProfesoresCVA.csv");
+
+			while (!archivoProfesores.eof())
+			{
+				getline(archivoProfesores, dato);
+				cout << dato << endl;
+			}
+			archivoProfesores.close();
+			break;
+		}
+		default:
+		{
+			break;
+		}
+	}
+}
+
+void GruposMateriaEspecifica(string materia) 
+{
+	bool materiaExiste = false;
+
+	ifstream archivoGrupos;
+	string dato;
+	string claveMateria;
+	string header;
+
+
+	archivoGrupos.open("GruposCVA.csv");
+	getline(archivoGrupos, header);
+	cout <<endl<< header << endl;
+
+	while (!archivoGrupos.eof())
+	{
+		getline(archivoGrupos, dato);
+		claveMateria= dato.substr(0, dato.find(','));
+		if (claveMateria == materia)
+		{
+			
+			cout << dato << endl;
+			materiaExiste = true;
+		}
+	}
+	archivoGrupos.close();
+
+	if (!materiaExiste) 
+	{
+		cout << "Lo siento esta materia no tiene ningun grupo dado de alta" << endl;
+	}
+}
+
+void GruposProfesorEspecifico(string profesor) 
+{
+	bool maestroExiste = false;
+	ifstream archivoGrupos;
+	string dato;
+	string dato2;
+	string claveMateria;
+	string header;
+	archivoGrupos.open("GruposCVA.csv");
+	getline(archivoGrupos, header);
+	cout << endl << header << endl;
+
+
+	while (!archivoGrupos.eof())
+	{
+		getline(archivoGrupos, dato);
+		dato2 = dato;
+		dato2.erase(0, dato2.find(',')+1);
+		dato2 = dato2.substr(0, dato2.find(','));
+		if (dato2 == profesor)
+		{
+			cout << dato << endl;
+			maestroExiste = true;
+		}
+		
+	}
+	archivoGrupos.close();
+
+	if (!maestroExiste)
+	{
+		cout << "Lo siento esta profesor no tiene ningun grupo a su mando" << endl;
+	}
+}
---
+++ Tarea1_ManejoDeArchivos/Source.cpp
@@ -0,0 +1,61 @@
+#include<string>
+#include<fstream>
+#include <locale.h>
+#include <clocale>
+#include"Catalagos.h"
+#include<iostream>
+
+using namespace std;
+
+int main() 
+{
+	setlocale(LC_ALL, "");
+	int decision=0;
+	cout << "Hola bienvenido a la base de datos de TEC CVA" << endl;
+
+	while (decision != 5) 
+	{
+		cout << "\nQue deseas hacer?" << endl;
+		cout << "1.- Ver catalogos" << endl;
+		cout << "2.- Mostrar todos los grupos de una materia especifica" << endl;
+		cout << "3.- Mostrar todos los grupos de un profesor" << endl;
+
+
+		cout << "5.- Salir" << endl;
+		cin >> decision;
+		cout << endl;
+		switch (decision)
+		{
+		case(1):
+		{
+			MostrarCatalogos();
+			break;
+		}
+		case(2):
+		{
+			cout << "Porfavor ingresa la clave de la materia que deseas buscar" << endl;
+			string materia;
+			cin >> materia;
+			GruposMateriaEspecifica(materia);
+			break;
+		}
+		case(3):
+		{
+			cout << "Porfavor ingresa la nomina del profesor a buscar" << endl;
+			string profesor;
+			cin >> profesor;
+			GruposProfesorEspecifico(profesor);
+			break;
+		}
+		default:
+		{
+			break;
+		}
+		}
+	}
+	
+
+	
+	system("pause");
+
+}
